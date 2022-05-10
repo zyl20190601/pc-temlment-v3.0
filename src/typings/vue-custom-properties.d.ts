0bb11d0b76577b35d739b88f201a59b72f650db6
张燕良 <zhangyanliang@kfang.com>
2022-05-10 17:58:00 +0800

    fix: init

--- src/typings/vue-custom-properties.d.ts
+++ src/typings/vue-custom-properties.d.ts
@@ -4,5 +4,6 @@ import { RootState } from '@/store/typings/state-type'
 declare module '@vue/runtime-core' {
   interface ComponentCustomProperties {
     $store: Store<RootState>
+    getImgSrc: any
   }
 }
